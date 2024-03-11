414ea505736b66bcb1f85a3d3571b2a2272834b2
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-03-11 20:43:37 +0100

    cos tam dziala

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload testtwest.
+          Edit <code>src/App.js</code> kocham disa i jego "forda".
         </p>
         <a
           className="App-link"
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<h1>Hello, world!</h1>);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
