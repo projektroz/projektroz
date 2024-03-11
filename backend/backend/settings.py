5cc03190cbe9be40588dc670952e6ddaa2977393
Szymek <szymacha22@gmail.com>
2024-03-11 20:08:39 +0100

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "projektRoz",
 ]
 
 ROOT_URLCONF = "projektRoz.urls"
