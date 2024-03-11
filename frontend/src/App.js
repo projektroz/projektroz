a0cae5935f30e04c0bda438ce4f64a2cb803c96f
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-03-11 21:01:29 +0100

    działa react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -112,6 +112,7 @@ function App() {
     );
   }
   return (
+<<<<<<< Updated upstream
     <div>
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -169,6 +170,23 @@ function App() {
         </div>
       )
     }
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> kocham disa i jego "mustanga".
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> Stashed changes
     </div>
   );
 }
