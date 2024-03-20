c6db3fbf22c4c8e7ba62b6dbaff7d13344b4829d
kubas8111 <kubas8111@interia.pl>
2024-03-20 16:10:52 +0100

    notes added

--- backend/projektRoz/apiViews/NotesApiView.py
+++ backend/projektRoz/apiViews/NotesApiView.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
-from projektRoz.models import Notes
+from projektRoz.models import Notes, Child, FosterCareer
 from projektRoz.serializer import NotesSerializer
 
 class NotesApiView(APIView):
@@ -24,15 +24,31 @@ class NotesApiView(APIView):
         Returns:
         - Response: The serialized note(s) data.
         """
-        if note_id is None:
-            note = Notes.objects.all().order_by('id')
-        else:
-            note = Notes.objects.filter(id=note_id)
+        if note_id:
+            note = Notes.objects.get(id=note_id)
+            child = Child.objects.get(note = note)
+            fosterCareer = FosterCareer.objects.get(id = request.user.id)
             
-        serializer = NotesSerializer(note, many=True)
-        
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
+            if child.foster_career == fosterCareer:
+                serializer = NotesSerializer(note, many = False)
+
+                return Response(serializer.data, status = status.HTTP_200_OK)
+        else:
+            fosterCareer = FosterCareer.objects.get(id = request.user.id)
+            children = Child.objects.filter(foster_career = fosterCareer)
+            notes = Notes.objects.all()
+            ret = []
+
+            for child in children:
+                for note in notes:
+                    if child.note == note:
+                        ret.append(note)
+            
+            serializer = NotesSerializer(ret, many=True)
+
+            return Response(serializer.data, status = status.HTTP_200_OK)
+
+
     def post(self, request, *args, **kwargs):
         """
         Create a new note.
@@ -51,7 +67,7 @@ class NotesApiView(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def put(self, request, note_id, *args, **kwargs):
+    def put(self, request, note_id = None, *args, **kwargs):
         """
         Update an existing note.
 
@@ -62,25 +78,24 @@ class NotesApiView(APIView):
         Returns:
         - Response: The serialized note data if successful, or the error message if validation fails.
         """
-        note = Notes.objects.get(id=note_id)
-        
-        if note is not None:
-            serializer = NotesSerializer(note, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                
-                return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        elif note is None:
-            serializer = NotesSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+        if note_id:
+            note = Notes.objects.get(id=note_id)
+            child = Child.objects.get(note = note)
+            fosterCareer = FosterCareer.objects.get(id = request.user.id)
+            
+            if child.foster_career == fosterCareer:
+                serializer = NotesSerializer(note, data = request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status = status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+            serializer = NotesSerializer(data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     def delete(self, request, note_id, *args, **kwargs):
         """
         Delete a note.
@@ -93,11 +108,12 @@ class NotesApiView(APIView):
         - Response: No content if successful, or the error message if the note is not found.
         """
         note = Notes.objects.get(id=note_id)
+        child = Child.objects.get(note = note)
+        fosterCareer = FosterCareer.objects.get(id = request.user.id)
         
-        if note is not None:
+        if child.foster_career == fosterCareer:
             note.delete()
             
             return Response(status=status.HTTP_204_NO_CONTENT)
         
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
