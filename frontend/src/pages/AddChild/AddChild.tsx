d3f33a905fb21ee44984a0bf896c80b8353759fc
EskamXD <58010243+EskamXD@users.noreply.github.com>
2024-04-10 18:36:46 +0200

    Update AddChild.tsx

--- frontend/src/pages/AddChild/AddChild.tsx
+++ frontend/src/pages/AddChild/AddChild.tsx
@@ -22,23 +22,19 @@ function AddChild() {
   const [addressRegisteredCountry, setAddressRegisteredCountry] = useState("");
   const [addressRegisteredCity, setAddressRegisteredCity] = useState("");
   const [addressRegisteredStreet, setAddressRegisteredStreet] = useState("");
-  const [addressRegisteredPostalCode, setAddressRegisteredPostalCode] = useState("");
-  const [addressRegisteredHouseNumber, setAddressRegisteredHouseNumber] = useState("");
+  const [addressRegisteredPostalCode, setAddressRegisteredPostalCode] =
+    useState("");
+  const [addressRegisteredHouseNumber, setAddressRegisteredHouseNumber] =
+    useState("");
 
   const [addressCurrentCountry, setAddressCurrentCountry] = useState("");
   const [addressCurrentCity, setAddressCurrentCity] = useState("");
   const [addressCurrentStreet, setAddressCurrentStreet] = useState("");
   const [addressCurrentPostalCode, setAddressCurrentPostalCode] = useState("");
-  const [addressCurrentHouseNumber, setAddressCurrentHouseNumber] = useState("");
+  const [addressCurrentHouseNumber, setAddressCurrentHouseNumber] =
+    useState("");
 
-  const setters[] = [
-    setName, setSurname, setBirthDate, setBirthPlace, setPesel, setAdmissionDate, setCourtDecision,
-    setAddressRegisteredCountry, setAddressRegisteredCity, setAddressRegisteredStreet, setAddressRegisteredPostalCode, setAddressRegisteredHouseNumber,
-    setAddressCurrentCountry, setAddressCurrentCity, setAddressCurrentStreet, setAddressCurrentPostalCode, setAddressCurrentHouseNumber,
-    setMotherName, setMotherSurname, setFatherName, setFatherSurname
-  ]
   const [error, setError] = useState<string>("");
-
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -64,7 +60,7 @@ function AddChild() {
   interface DataInput {
     inputLabel: string;
     placeholder: string;
-    type: 'date' | 'text';
+    type: "date" | "text";
     regex?: string;
   }
 
@@ -72,34 +68,83 @@ function AddChild() {
     [
       { inputLabel: "Imię", placeholder: "Wpisz imię", type: "text" },
       { inputLabel: "Nazwisko", placeholder: "Wpisz nazwisko", type: "text" },
-      { inputLabel: "Data urodzenia", placeholder: "Wpisz datę urodzenia", type: "date" },
-      { inputLabel: "Miejsce urodzenia", placeholder: "Wpisz miejsce urodzenia", type: "text" },
-      { inputLabel: "PESEL", placeholder: "Wpisz PESEL", type: "text", regex: "[0-9]{11}$" },
-      { inputLabel: "Data przyjęcia", placeholder: "Wpisz datę przyjęcia", type: "date" },
-      { inputLabel: "Decyzja sądu", placeholder: "Wpisz decyzję sądu", type: "text" },
+      {
+        inputLabel: "Data urodzenia",
+        placeholder: "Wpisz datę urodzenia",
+        type: "date",
+      },
+      {
+        inputLabel: "Miejsce urodzenia",
+        placeholder: "Wpisz miejsce urodzenia",
+        type: "text",
+      },
+      {
+        inputLabel: "PESEL",
+        placeholder: "Wpisz PESEL",
+        type: "text",
+        regex: "[0-9]{11}$",
+      },
+      {
+        inputLabel: "Data przyjęcia",
+        placeholder: "Wpisz datę przyjęcia",
+        type: "date",
+      },
+      {
+        inputLabel: "Decyzja sądu",
+        placeholder: "Wpisz decyzję sądu",
+        type: "text",
+      },
     ],
     [
-      {inputLabel: "Kraj", placeholder: "Wpisz kraj", type: "text"},
-      {inputLabel: "Miastio", placeholder: "Wpisz miasto", type: "text"},
-      {inputLabel: "Ulica", placeholder: "Wpisz ulicę", type: "text"},
-      {inputLabel: "Kod pocztowy", placeholder: "Wpisz kod pocztowy", type: "text"},
-      {inputLabel: "Numer domu", placeholder: "Wpisz numer domu", type: "text"},
+      { inputLabel: "Kraj", placeholder: "Wpisz kraj", type: "text" },
+      { inputLabel: "Miastio", placeholder: "Wpisz miasto", type: "text" },
+      { inputLabel: "Ulica", placeholder: "Wpisz ulicę", type: "text" },
+      {
+        inputLabel: "Kod pocztowy",
+        placeholder: "Wpisz kod pocztowy",
+        type: "text",
+      },
+      {
+        inputLabel: "Numer domu",
+        placeholder: "Wpisz numer domu",
+        type: "text",
+      },
     ],
     [
-      {inputLabel: "Kraj", placeholder: "Wpisz kraj", type: "text"},
-      {inputLabel: "Miastio", placeholder: "Wpisz miasto", type: "text"},
-      {inputLabel: "Ulica", placeholder: "Wpisz ulicę", type: "text"},
-      {inputLabel: "Kod pocztowy", placeholder: "Wpisz kod pocztowy", type: "text"},
-      {inputLabel: "Numer domu", placeholder: "Wpisz numer domu", type: "text"},
+      { inputLabel: "Kraj", placeholder: "Wpisz kraj", type: "text" },
+      { inputLabel: "Miastio", placeholder: "Wpisz miasto", type: "text" },
+      { inputLabel: "Ulica", placeholder: "Wpisz ulicę", type: "text" },
+      {
+        inputLabel: "Kod pocztowy",
+        placeholder: "Wpisz kod pocztowy",
+        type: "text",
+      },
+      {
+        inputLabel: "Numer domu",
+        placeholder: "Wpisz numer domu",
+        type: "text",
+      },
     ],
     [
-      {inputLabel: "Imie Matki", placeholder: "Wpisz imię matki", type: "text"},
-      {inputLabel: "Nazwisko Matki", placeholder: "Wpisz nazwisko matki", type: "text"},
+      {
+        inputLabel: "Imie Matki",
+        placeholder: "Wpisz imię matki",
+        type: "text",
+      },
+      {
+        inputLabel: "Nazwisko Matki",
+        placeholder: "Wpisz nazwisko matki",
+        type: "text",
+      },
     ],
     [
-      {inputLabel: "Imie Ojca", placeholder: "Wpisz imię ojca", type: "text"},
-      {inputLabel: "Nazwisko Ojca", placeholder: "Wpisz nazwisko ojca", type: "text"},
-    ]
+      { inputLabel: "Imie Ojca", placeholder: "Wpisz imię ojca", type: "text" },
+      {
+        inputLabel: "Nazwisko Ojca",
+        placeholder: "Wpisz nazwisko ojca",
+        type: "text",
+      },
+    ],
   ];
 
   return (
@@ -107,12 +152,10 @@ function AddChild() {
       <Rectangle links={links}>
         <div className="child-content">
           <h2>Dodaj dziecko</h2>
-            <form onSubmit={handleSubmit}>
-              <ChildDataCard
-                dataSets={dataSets}
-              />
-              <button type="submit">Dodaj dziecko</button>
-            </form>
+          <form onSubmit={handleSubmit}>
+            <ChildDataCard dataSets={dataSets} />
+            <button type="submit">Dodaj dziecko</button>
+          </form>
           <div className="error">{error}</div>
         </div>
       </Rectangle>
