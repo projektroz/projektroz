60c082aa6e84ad575bcf49ce7fedc07129dc145c
Szymek <szymacha22@gmail.com>
2024-04-18 13:25:02 +0200

    PROJ-46 <swagger>

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Detailed documentation of all API endpoints in the projektRoz",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("projektRoz/", include("projektRoz.urls")),
     path('api-auth/', include('rest_framework.urls')),
-    path("admin/", admin.site.urls),
 ]
--- backend/projektRoz/apiViews/RegistrationView.py
+++ backend/projektRoz/apiViews/RegistrationView.py
@@ -2,11 +2,23 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..serializer import UserRegistrationSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class RegistrationView(APIView):
     """
     Creates the user.
     """
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('username', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('password', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('passwordRepeat', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('email', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('first_name', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('last_name', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+        ],
+    )
     def post(self, request, format='json'):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
