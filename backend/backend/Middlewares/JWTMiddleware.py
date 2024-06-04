24868a874fa2715d77df58d07738f7e707be63d7
Szymek <szymacha22@gmail.com>
2024-06-05 00:24:54 +0200

    formatted

--- backend/backend/Middlewares/JWTMiddleware.py
+++ backend/backend/Middlewares/JWTMiddleware.py
@@ -1,11 +1,10 @@
-from rest_framework_simplejwt.exceptions import TokenError
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+
 class JWTMiddleware(MiddlewareMixin):
-    """
-    Middleware class for JWT authentication.
+    """Middleware class for JWT authentication.
 
     This middleware validates JWT tokens for incoming requests and sets the authenticated user in the request object.
 
@@ -14,15 +13,13 @@ class JWTMiddleware(MiddlewareMixin):
 
     Methods:
         process_request(request): Processes the incoming request and performs JWT token validation.
-
     """
 
     def __init__(self, get_response):
         self.get_response = get_response
-        
+
     def process_request(self, request):
-        """
-        Processes the incoming request and performs JWT token validation.
+        """Processes the incoming request and performs JWT token validation.
 
         Args:
             request (HttpRequest): The incoming request object.
@@ -31,37 +28,40 @@ class JWTMiddleware(MiddlewareMixin):
             None or HttpResponse or JsonResponse: Returns None if the request path is exempted from token validation.
                                                   Returns an HttpResponse with an appropriate error message if the token is missing or invalid.
                                                   Returns None if the token is valid and sets the authenticated user in the request object.
-
         """
-        exempt_paths = ['/login/', '/register/', '/refresh/', '/swagger/', '/redoc/', '/swagger.json']
+        exempt_paths = [
+            "/login/",
+            "/register/",
+            "/refresh/",
+            "/swagger/",
+            "/redoc/",
+            "/swagger.json",
+        ]
 
         if request.path in exempt_paths:
-            return None  
-        if request.path.startswith('/admin/'):
+            return None
+        if request.path.startswith("/admin/"):
             return None
 
         auth = JWTAuthentication()
         header = auth.get_header(request)
-        
+
         if header is None:
-            return HttpResponse('No token provided', status=406)
-        
+            return HttpResponse("No token provided", status=406)
+
         else:
             raw_token = auth.get_raw_token(header)
-            if raw_token is None or raw_token == b'':
-                return HttpResponse('No token provided', status=406)
+            if raw_token is None or raw_token == b"":
+                return HttpResponse("No token provided", status=406)
 
         try:
             validated_token = auth.get_validated_token(raw_token)
             request.user = auth.get_user(validated_token)
         except Exception as e:
             error_detail = str(e)
-            response_data = {
-                'detail': 'Token error',
-                'error': error_detail
-            }
+            response_data = {"detail": "Token error", "error": error_detail}
 
-            if 'token_not_valid' in str(error_detail):
+            if "token_not_valid" in str(error_detail):
                 return JsonResponse(response_data, status=401)
             return JsonResponse(response_data, status=500)
         return None
--- backend/projektRoz/urls.py
+++ backend/projektRoz/urls.py
@@ -1,25 +1,22 @@
-from django.urls import path
 from django.contrib import admin
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .apiViews.RegistrationView import RegistrationView
-from projektRoz.apiViews import (
-    AddressApiView,
-    ChildrenApiView,
-    ParentApiView,
-    NotesApiView,
-    FosterCarerApiView,
-    SiblingsApiView,
-    DocumentsApiView,
-    FileApiView,
-)
-
-from drf_yasg.views import get_schema_view
+from django.urls import path
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from projektRoz.apiViews import (AddressApiView, ChildrenApiView,
+                                 DocumentsApiView, FileApiView,
+                                 FosterCarerApiView, NotesApiView,
+                                 ParentApiView, SiblingsApiView)
+
+from .apiViews.RegistrationView import RegistrationView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
-        default_version='v1',
+        default_version="v1",
         description="Detailed documentation of all API endpoints in the projektRoz",
     ),
     public=True,
@@ -27,45 +24,51 @@ schema_view = get_schema_view(
 )
 
 
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
-    
-    path('api/children/', ChildrenApiView.as_view(), name='children'),
-    path('api/children/<int:child_id>', ChildrenApiView.as_view(), name='children'),
-
-    path('api/parent/<str:role>/', ParentApiView.as_view(), name='parent'),
-    path('api/parent/<str:role>/<int:parent_id>', ParentApiView.as_view(), name='parent'),
-    
-    path('api/notes/', NotesApiView.as_view(), name='notes'),
-    path('api/notes/<int:note_id>', NotesApiView.as_view(), name='notes'),
-    
-    path('api/address/<str:type>/', AddressApiView.as_view(), name='address'),
-    path('api/address/<str:type>/<int:address_id>', AddressApiView.as_view(), name='address'),
-    
-    path('api/foster_carer/', FosterCarerApiView.as_view(), name='foster_carer'),
-    path('api/foster_carer/<int:foster_carer_id>', FosterCarerApiView.as_view(), name='foster_carer'),
-    
-    path('api/siblings/', SiblingsApiView.as_view(), name='siblings'),
-    path('api/siblings/<int:siblings_id>', SiblingsApiView.as_view(), name='siblings'),
-    
+    path("", views.index, name="index"),
+    path("admin/", admin.site.urls),
+    path("api/children/", ChildrenApiView.as_view(), name="children"),
+    path("api/children/<int:child_id>", ChildrenApiView.as_view(), name="children"),
+    path("api/parent/<str:role>/", ParentApiView.as_view(), name="parent"),
+    path(
+        "api/parent/<str:role>/<int:parent_id>", ParentApiView.as_view(), name="parent"
+    ),
+    path("api/notes/", NotesApiView.as_view(), name="notes"),
+    path("api/notes/<int:note_id>", NotesApiView.as_view(), name="notes"),
+    path("api/address/<str:type>/", AddressApiView.as_view(), name="address"),
+    path(
+        "api/address/<str:type>/<int:address_id>",
+        AddressApiView.as_view(),
+        name="address",
+    ),
+    path("api/foster_carer/", FosterCarerApiView.as_view(), name="foster_carer"),
+    path(
+        "api/foster_carer/<int:foster_carer_id>",
+        FosterCarerApiView.as_view(),
+        name="foster_carer",
+    ),
+    path("api/siblings/", SiblingsApiView.as_view(), name="siblings"),
+    path("api/siblings/<int:siblings_id>", SiblingsApiView.as_view(), name="siblings"),
     # path('api/category/', CategoryApiView.as_view(), name='category'),
     # path('api/category/<int:category_id>', CategoryApiView.as_view(), name='category'),
-    
-    path('api/documents/', DocumentsApiView.as_view(), name='documents'),
-    path('api/documents/<int:document_id>', DocumentsApiView.as_view(), name='documents'),
-    
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-    path('upload/', FileApiView.as_view(), name='upload_map'),
-    path('upload/<str:id>', FileApiView.as_view(), name='upload_map')
+    path("api/documents/", DocumentsApiView.as_view(), name="documents"),
+    path(
+        "api/documents/<int:document_id>", DocumentsApiView.as_view(), name="documents"
+    ),
+    path("register/", RegistrationView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("upload/", FileApiView.as_view(), name="upload_map"),
+    path("upload/<str:id>", FileApiView.as_view(), name="upload_map"),
 ]
